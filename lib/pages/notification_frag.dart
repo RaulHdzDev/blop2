fef868c1ea6e929d96b9e2891c97b5f861054327
Jose Medellin <josekaboxd@hotmail.com>
2020-03-29 23:22:05 -0600

    Se agrego carpeta widgetNotification Se grego archivos como listHistory, list Creacion de todos los componentes de notificacion

--- lib/pages/notification_frag.dart
+++ lib/pages/notification_frag.dart
@@ -1,11 +1,33 @@
+import 'package:bloop/animation/FadeAnimation.dart';
+import 'package:bloop/pages/notification_widget/header.dart';
+import 'package:bloop/pages/notification_widget/list.dart';
+import 'package:bloop/pages/notification_widget/listHistory.dart';
 import 'package:flutter/material.dart';
 
 class notification_fragment extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("FRAGMENT NOTIFICATION",textScaleFactor: 2.0,style: TextStyle(color: Colors.black),),
-    );
+    return Scaffold(
+        body: SafeArea(
+      bottom: true,
+      child: Column(children: <Widget>[
+        Header(),
+        new Container( //Texto de rencientes
+          child: Row(
+            children: <Widget>[
+              Text(
+                " Recientes",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'SFUIDisplay',
+                    color: Colors.black45),
+              ),
+            ],
+          ),
+        ),
+        new Expanded(child: ListNotification(), ),
+        new Expanded (child: HistoryList())
+      ]),
+    ));
   }
 }
